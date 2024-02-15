141fdc8a7efe0ac0e7d4ca1ffc09b5d2e0e0011b
littleeyfa <160107997+littleeyfa@users.noreply.github.com>
2024-02-15 15:44:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <tittle>Source Code Management</tittle>
+     </head>
+  <body>
+  <h1>Source Code Management</h1>
+<table>
+  <tr> 
+  <th>BIL</th>
+  <th>NAMA</th>
+    </tr>
+  <tr>
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+    <td>4</td>
+  </tr>
+</table>
+  </body>
+</html>
